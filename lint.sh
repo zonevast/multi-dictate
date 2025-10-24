23355d4666be3caa52b431ec56e6fc2e1f390d65
Costa Shulyupin <costa.shul@redhat.com>
2025-10-24 10:41:43 +0300

    *lint, no black

--- lint.sh
+++ lint.sh
@@ -9,9 +9,14 @@ sed -i 's/[[:space:]]*$//' $files README.md *.sh 2>/dev/null
 
 # Format and check
 isort $files 2>/dev/null || echo "Install isort: pip install isort"
-black --line-length 100 $files 2>/dev/null || echo "Install black: pip install black"
-flake8 --max-line-length=100 --ignore=E203,W503 $files 2>/dev/null || echo "Install flake8: pip install flake8"
+#black --line-length 100 --skip-string-normalization $files 2>/dev/null || echo "Install black: pip install black"
+
+#autopep8 --max-line-length 100 --in-place  $files
+
+#flake8 --max-line-length=100 --ignore=E203,W503 $files
+
+#pylint -d C0415,W1203,R1722
 
 sort requirements.txt -o requirements.txt
 
-ctags $t
+ctags $files
